9dcfe1531fc35cd2eab652535b559e6b7afa5f47
luan3642 <luan3642@hotmail.com>
2020-10-15 21:07:50 -0300

    adicionando comentarios

--- somaNumeros.hs
+++ somaNumeros.hs
@@ -1,2 +1,11 @@
 soma 1 = 1
-soma n = soma(n-1) + n 
+soma n = soma(n - 1) + n
+
+{- soma 3  = soma (2) + 3   -}
+
+{- soma 2  = soma (1) + 2  -}
+
+{- soma 1 = soma(1) + 1 -}
+
+
+{- 3 = 3 + 2 + 1 = 6 -}
